dc8d49f371665598ab4928836f202b30f1de40df
garen.chen@oocl.com <garen.chen@oocl.com>
2019-12-03 18:56:50 +0800

    validate filterchain execute sequence(default)

---
+++ src/main/java/com/fisrtspringboot/filter/AiUserFilter.java
@@ -0,0 +1,30 @@
+package com.fisrtspringboot.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName = "aiFilter", urlPatterns = "/*")
+public class AiUserFilter implements Filter {
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    System.out.println("----------Ai Filter init------------");
+  }
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    System.out.println("----------Ai Filter doFilter------------");
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
+
+  @Override
+  public void destroy() {
+    System.out.println("----------Ai Filter destroy------------");
+  }
+}
